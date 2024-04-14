e626a19fec4be54a75b7afe71856065782d625a0
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-04-14 15:19:26 -0400

    Updated folder structure.

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require('express');
 // Setup express app.
 const app = express();
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    const page = '/public/Login.html';
+    res.sendFile(__dirname + page);
+});
 
 const port = 8000;
-
 app.listen(port, function() {
     let address = `http://localhost:${port}`;
     console.log(`Listening on ${address}`);
