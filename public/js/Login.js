fa35310a639ef4e50f1dcfb1f7b99ee0a4bfa120
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-04-14 23:32:57 -0400

    Added login form and first script

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,1 +1,19 @@
-console.log('Loaded login page.')
+async function login() {
+    const username = document.getElementById("usernameInput").value
+    const password = document.getElementById("passwordInput").value
+    
+    fetch("https://localhost:7166/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: hashedPassword
+        })
+      })
+}
+
+async function signup() {
+    //lol we decided not to do this
+}
