d618d645eaaef5d002b6d55f664e613d29bef669
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-04-15 23:08:57 -0400

    Add basic validation.

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,6 +1,9 @@
 async function login() {
-    const username = document.getElementById("usernameInput").value
-    const password = document.getElementById("passwordInput").value
+    const username = $('#usernameInput').val()
+    const password = $('#passwordInput').val()
+
+    console.log(`Username: ${username}`)
+    console.log(`Password: ${[password]}`)
     
     fetch("https://localhost:7166/api/auth/login", {
         method: "POST",
@@ -12,8 +15,18 @@ async function login() {
           password: password
         })
       })
-}
-
-async function signup() {
-    //lol we decided not to do this
+      .then(resp =>  {
+        if (resp.ok) {
+            // Redirect to car list page.
+            console.log('Login successful...')
+            $(`#signinMessages`).text(` `).hide()
+        } else {
+            // Display error message.
+            console.log(`Error. Login status: ${resp.status}`)
+            $('#signinMessages').text(`❌ Invalid username or password.`).show()
+        }
+      })
+      .catch(error => {
+        console.log(error)
+    });
 }
