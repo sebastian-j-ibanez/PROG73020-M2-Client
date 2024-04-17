09063b4bda64ff8b80931892440cbb97522dd8ef
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-04-17 08:17:05 -0400

    Added a bunch of stuff idk

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -1,17 +1,24 @@
-$(function(){
-    $("#header").load("Header.html"); 
-    });
+$(function () {
+    $("#header").load("Header.html");
+});
 
-function deleteMethod() {
-    console.log('To be implemented...')
+async function deleteBooking(id) {
+    await fetch(`https://localhost:7166/api/booking/${id}`, {
+        method: 'DELETE'
+    }).then(res => {
+        loadBookings();
+    }).catch(err => {
+        console.log(err);
+    })
+
 }
 
-function getCookie(cookieName){
+function getCookie(cookieName) {
     const name = cookieName + "=";
     const decodedCookie = decodeURIComponent(document.cookie);
     const cookieArray = decodedCookie.split(';');
-    
-    for(let i = 0; i < cookieArray.length; i++) {
+
+    for (let i = 0; i < cookieArray.length; i++) {
         let cookie = cookieArray[i];
         while (cookie.charAt(0) == ' ') {
             cookie = cookie.substring(1);
@@ -22,31 +29,47 @@ function getCookie(cookieName){
     }
     return "";
 }
-const username = getCookie("vitesse_username");
-const bookingList =  $('#bookingList');
 
-fetch(`https://localhost:7166/api/booking/user/${username}`)
-.then(resp => {
-    return resp.json()
-})
-.then(body => {
-    body.forEach(function(booking) {
-        let carName = `${booking.carBooked.make} ${booking.carBooked.model}`
-        let container = $('<div>').addClass('')
-        bookingList.append(container)
-        container.html(`<div style="display: flex; justify-content: space-between; border: solid 1px black; border-radius: 20%;">
-        <div>
-            <h2>${carName}</h2>
-            <p><span>${booking.startDate}</span> to <span>${booking.endDate}</span></p>
-        </div>
-        <div>
-            <button onclick="deleteMethod()">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zm2-4h2V8H9zm4   0h2V8h-2z"/></svg>
-            </button>
-        </div>
-        </div>`)
-    })
-})
-.catch(error => {
-    console.log(error)
-});
+async function loadBookings() {
+    const username = getCookie("vitesse_userauth");
+    const bookingList = $('#bookingList');
+
+    bookingList.innerHTML = '';
+
+    await fetch(`https://localhost:7166/api/booking/user/${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(resp => {
+        return resp.json()
+    }).then(body => {
+
+        console.log(body);
+
+        body.forEach(function (booking) {
+
+            let startDate = new Date(booking.startTime).toDateString();
+            let endDate = new Date(booking.endTime).toDateString();
+
+            let carName = `${booking.carBooked.make} ${booking.carBooked.model}`
+            let container = $('<div>').addClass('')
+            bookingList.append(container)
+            container.html(`<div style="display: flex; justify-content: space-between; align-items: center; border: solid 2px black; border-radius: 10px; padding: 10px; margin-bottom: 10px;">
+            <div>
+                <h2>${carName}</h2>
+                <p style="margin: 0px"><span>${startDate}</span> to <span>${endDate}</span></p>
+            </div>
+            <div>
+                <button onclick="deleteBooking(${booking.BookingId})" style="border: none; background-color: transparent;" class="deleteButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"/></svg>
+                </button>
+            </div>
+            </div>`)
+        })
+    }).catch(error => {
+        console.log(error)
+    });
+}
+
+loadBookings();
