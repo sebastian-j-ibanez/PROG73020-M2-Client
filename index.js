f9e4ad58dd7efb27c1aa580400bb3fc7b1c5bb3e
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-04-17 03:35:38 -0400

    Added booking page to routing

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.get("/cars", function(req, res) {
     console.log(`SERVE: ${page}`);
 });
 
+app.get("/booking", function(req, res) {
+    const page = '/public/Booking.html';
+    res.sendFile(__dirname + page);
+    console.log(`SERVE: ${page}`);
+});
+
 // Listen over specified port.
 const port = 8000;
 app.listen(port, function() {
